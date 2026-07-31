a228a7dc8119c4901732af43e1145e1335fe5c94
uminocelo <marcelo.mdl.dev@gmail.com>
2026-07-30 21:08:03 -0300

    chore: add host

--- include/net_utils.h
+++ include/net_utils.h
@@ -8,12 +8,32 @@
 #ifdef _WIN32
     #include <winsock2.h>
     #include <ws2tcpip.h>
-
+    #include <windows.h>
     #pragma comment(lib, "ws2_32.lib")
 
     typedef SOCKET socket_fd_t;
     #define CLOSE_SOCKET(s) closesocket(s)
     #define VALID_SOCKET(s) ((s) != INVALID_SOCKET)
+
+    typedef HANDLE anthill_thread_t;
+    typedef CRITICAL_SECTION anthill_mutex_t;
+    #define ANTHILL_THREAD_FUNC DWORD WINAPI
+    #define ANTHILL_THREAD_RETURN DWORD
+
+    static inline void anthill_mutex_init(anthill_mutex_t *m) { InitilizeCriticalSection(m); }
+    static inline void anthill_mutex_lock(anthill_mutex_t *m) { EnterCriticalSection(m); }
+    static inline void anthill_mutex_unlock(anthill_mutex_t *m) { LeaveCriticalSection(m); }
+    static inline void anthill_mutex_destroy(anthill_mutex_t *m) { DeleteCriticalSection(m); }
+
+    static inline int anthill_thread_create(anthill_thread_t *t, ANTHILL_THREAD_FUNC (*func)(void*), void *arg){
+        *t = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)func, arg, 0, NULL);
+        return (*t == NULL) ? 1 : 0;
+    }
+
+    static inline void anthill_thread_join(anthill_thread_t t) {
+        WaitForSingleObject(t, INFINITE);
+        CloseHandle(t);
+    }
 
     static inline void init_network_workers(void) {
         WSADATA wsaData;
@@ -34,18 +54,32 @@
     #include <netinet/in.h>
     #include <arpa/inet.h>
     #include <unistd.h>
+    #include <pthread.h>
 
     typedef int socket_fd_t;
     #define CLOSE_SOCKET(s) close(s)
     #define VALID_SOCKET(s) ((s) >= 0)
 
+    typedef pthread_t anthill_thread_t;
+    typedef pthread_mutex_t anthill_mutex_t;
+    #define ANTHILL_THREAD_FUNC void*
+    #define ANTHILL_THREAD_RETURN void*
+
+    static inline void anthill_mutex_init(anthill_mutex_t *m) { pthread_mutex_init(m, NULL); }
+    static inline void anthill_mutex_lock(anthill_mutex_t *m) { pthread_mutex_lock(m); }
+    static inline void anthill_mutex_unlock(anthill_mutex_t *m) { pthread_mutex_unlock(m); }
+    static inline void anthill_mutex_destroy(anthill_mutex_t *m) { pthread_mutex_destroy(m); }
+
+    static inline int anthill_thread_create(anthill_thread_t *t, ANTHILL_THREAD_FUNC (*func)(void*), void *arg) {
+        return pthread_create(t, NULL, func, arg);
+    }
+
+    static inline void anthill_thread_join(anthill_thread_t t) { pthread_join(t, NULL); }
     static inline void init_network_workers(void) {
         printf("[Anthill] POSIX network ready. Ants are ready.\n");
     }
 
-    static inline void cleanup_network_workers(void) {
-        //
-    }
+    static inline void cleanup_network_workers(void) {}
 
 #endif
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "../include/net_utils.h"
 
+#define NUM_THREADS 16
+#define MAX_PORT 65535
+
+typedef struct {
+    int squadron_id;
+    int start_port;
+    int end_port;
+    int *total_available;
+    anthill_mutex_t *mutex;
+} worker_args_t;
+
 bool check_port_availability(int port){
     socket_fd_t sock = socket(AF_INET, SOCK_STREAM, 0);
     if(!VALID_SOCKET(socket)){
@@ -18,23 +29,59 @@ bool check_port_availability(int port){
     return (result == 0);
 }
 
+ANTHILL_THREAD_FUNC ant_worker(void *arg) {
+    worker_args_t *args = (worker_args_t *)arg;
+    int local_count = 0;
+
+    for (int p = args->start_port; p <= args->end_port; p++){
+        if(check_port_availability(p)) {
+            local_count++;
+        }
+    }
+
+    anthill_mutex_lock(args->mutex);
+    *(args->total_available) += local_count;
+    anthill_mutex_unlock(args->mutex);
+
+    printf("    -> Squadron %02d completed (Ports %d to %d). \n", args->squadron_id, args->start_port, args->end_port);
+
+    return (ANTHILL_THREAD_RETURN)0;
+}
+
 int main(void) {
     printf("Spawing Anthil...\n");
     init_network_workers();
 
-    printf("Scout ants deploying to port 1 through 65335...\n");
-    int available_count = 0;
+    int total_available = 0;
+    anthill_mutex_t counter_mutex;
+    anthill_mutex_init(&counter_mutex);
 
-    for (int port = 1; port <=65535; port++){
-        if (check_port_availability(port)) {
-            printf("Port: %d: [AVAILABLE]\n", port);
-            available_count++;
-        }
+    anthill_thread_t threads[NUM_THREADS];
+    worker_args_t thread_args[NUM_THREADS];
+
+    int ports_per_thread = MAX_PORT / NUM_THREADS;
+
+    printf("\nDeploying %d ant squadrons...\n", NUM_THREADS);
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_args[i].squadron_id = i + 1;
+        thread_args[i].start_port = (i * ports_per_thread) + 1;
+
+        thread_args[i].end_port = (i == NUM_THREADS - 1) ? MAX_PORT : ((i + 1) * ports_per_thread);
+        thread_args[i].total_available = &total_available;
+        thread_args[i].mutex = &counter_mutex;
+
+        anthill_thread_create(&threads[i], ant_worker, &thread_args[i]);
     }
 
-    printf("Anthill dormant. All ants returned.\n");
-    printf("Total available ports on localhost: %d\n", available_count);
-    cleanup_network_workers();
+    for (int i = 0; i < NUM_THREADS; i++) {
+        anthill_thread_join(threads[i]);
+    }
 
+    anthill_mutex_destroy(&counter_mutex);
+    printf("Anthill dormant. All ants returned.\n");
+    printf("Total available ports on localhost: %d\n", total_available);
+
+    cleanup_network_workers();
     return 0;
 }
